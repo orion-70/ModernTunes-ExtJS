793dfbd8be856eb6f5a52c88c6cf5cadfd4aca69
towen <tyler.owen@agworks.net>
2020-04-24 08:23:14 -0500

    Initial commit

---
+++ app/desktop/src/view/Preview.js
@@ -0,0 +1,12 @@
+Ext.define('ModernTunes.view.Preview', {
+    extend: 'Ext.Dialog',
+    xtype: 'preview',
+    resizable: true,
+    closable: true,
+    bodyPadding: 16,
+    width: 640,
+    height: 480,
+    requires: [
+        'ModernTunes.view.Preview'
+    ]
+});
